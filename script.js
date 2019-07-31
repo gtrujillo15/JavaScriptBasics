b01766f670e2caaa017463e4e6ca7695121591f4
Gabriela T <gtrujillo15@gmail.com>
2019-07-30 22:03:44 -0700

    practiced and played around with some simple javascript

---
+++ script.js
@@ -0,0 +1,18 @@
+// declaring a variable
+var myName = "Gaby";
+console.log(myName);
+
+// number
+var num = 5;
+console.log(num);
+
+// adding a quote to a single line
+var myString = "He said \"it\'s fine\" so it's okay";
+console.log(myString);
+
+// adding one integer
+var hello = 5;
+hello++;
+console.log(hello);
+hello--;
+console.log(hello);
